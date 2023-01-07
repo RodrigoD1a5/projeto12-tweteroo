6b89ed11ac7e86e2dc96afff8a848d2ceaa4eba6
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2023-01-07 15:26:38 -0300

    Requisito GET /tweets

--- src/app.js
+++ src/app.js
@@ -7,9 +7,43 @@ server.use(json());
 
 const PORT = 5000;
 
-const usuarios = [];
+const usuarios = [
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+    },];
 
-const tweets = [];
+const tweets = [
+    {
+        username: "bobesponja",
+        tweet: "post 7"
+    },
+    {
+        username: "bobesponja",
+        tweet: "post 6"
+    },
+    {
+        username: "bobesponja",
+        tweet: "post 5"
+    },
+    {
+        username: "bobesponja",
+        tweet: "post 4"
+    },
+    {
+        username: "bobesponja",
+        tweet: "post 3"
+    },
+    {
+        username: "bobesponja",
+        tweet: "post 2"
+    },
+    {
+        username: "bobesponja",
+        tweet: "Post 1"
+    },
+
+];
 
 server.post("/sign-up", (req, res) => {
     const data = req.body;
@@ -25,6 +59,19 @@ server.post("/tweets", (req, res) => {
     }
     tweets.push(data);
     res.send("OK");
+});
+
+server.get("/tweets", (req, res) => {
+    const dezUltimosTweets = [];
+    for (let i = 1; dezUltimosTweets.length < 10; i++) {
+        if (i > tweets.length) break;
+        const { username } = tweets[tweets.length - i];
+        console.log(username);
+        const { avatar } = usuarios.find(u => u.username === username);
+        dezUltimosTweets.push({ ...tweets[tweets.length - i], avatar });
+    }
+    res.send(dezUltimosTweets);
+
 });
 
 server.listen(PORT, () => {
