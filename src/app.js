f91bd8e6b1233c65abdd5a4eb804cb818f7eec0e
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2023-01-07 14:48:38 -0300

    Requisito POST /sign-up

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
-import  express  from "express";
-import cors from "cors"
+import express, { json } from "express";
+import cors from "cors";
 
-const server = express(); 
+const server = express();
+server.use(cors());
+server.use(json());
 
-const PORT = 5000
+const PORT = 5000;
 
-server.listen(PORT, ()=> {
-    console.log(`Servidor funcionando na porta ${PORT}!`)
-})
+const usuarios = [];
+
+const tweets = [];
+
+server.post("/sign-up", (req, res) => {
+    const data = req.body;
+    usuarios.push(data);
+    res.send("OK!");
+});
+
+server.listen(PORT, () => {
+    console.log(`Servidor funcionando na porta ${PORT}!`);
+});
